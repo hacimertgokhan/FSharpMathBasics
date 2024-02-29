88a011bb325123d3b3ee3a47f106df50f493bf0e
hacimertgokhan <hacimertgokhan@gmail.com>
2024-03-01 02:01:40 +0300

    0.0.1

---
+++ Math/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+let rec do_math_funcs() =
+    let mutable bool = true;
+    while (bool) do
+        printfn "Numara giriniz ya da çıkış yapınız (*0*)"
+        let item = Console.ReadLine()
+        if item = "*0*" then
+            bool <- false
+        else 
+            let rec factorial x =
+                if x < 1 then 1
+                else x * factorial (x-1)
+            Printf.printfn "Faktöriyel: %d" (factorial (Int32.Parse(item)))
+            let rec sqrt x =
+                if x >= 1 then x * x
+                else 1
+            Printf.printfn "Karesi : %d" (sqrt (Int32.Parse(item)))
+            match Int32.TryParse(item) with
+            | (true, value) -> printfn ""
+            | _ -> printfn ""
+do_math_funcs();
